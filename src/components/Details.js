de095b31a173bd211744c1951fb19ce69d78198e
wcelery <horoschun.yura@yandex.ru>
2021-01-27 17:05:15 +0800

    fancy things

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Box, Heading, Text, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Text,
+  Stack,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react';
+import { v4 as uuidv4 } from 'uuid';
+import { VscDebugStackframeDot } from 'react-icons/vsc';
 import { isoCountries } from '../countryCodes';
 
 export default function Details({ data }) {
@@ -22,12 +32,26 @@ export default function Details({ data }) {
       >
         <Stack spacing={8}>
           <Heading fontSize="xl">
-            You searched for: {data.artists.map(a => a.name)} - {data.name}
+            You searched for: {data.artists.map(a => `${a.name} `)} -{' '}
+            {/* prettier was made this mess, not me ------------------^ */}
+            {data.name}
           </Heading>
-          <Text>
-            It is available in following countries:
-            {data.available_markets.map(code => ` ${getCountryName(code)},`)}
-          </Text>
+          {data.available_markets.length ? (
+            <Text>
+              It is available in {data.available_markets.length} following
+              countries:
+            </Text>
+          ) : (
+            <Text>It is not available</Text>
+          )}
+          <List spacing={1}>
+            {data.available_markets.map(code => (
+              <ListItem key={uuidv4()} fontSize="16px">
+                <ListIcon as={VscDebugStackframeDot} color="green.200" />
+                {getCountryName(code)}
+              </ListItem>
+            ))}
+          </List>
         </Stack>
       </Box>
     </div>
